a5a29ae16b4befb3df82668e78f56137f511cda7
Renan Vilas <renan.vilas@devoteam.com>
2025-05-19 18:00:08 +0100

    Laravel installation, FE for Job Listings, architecture(Model, Controller, Factory & Seeds), FE(npm, node, vide) in run dev, tailwindcss, Peste/PHPUnitytests run ------  Done

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Find Jobs</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    @Vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-800 text-white font-hanken-grotesk">
+    <div>
+        <nav class="px-10 flex justify-between items-center bg-black py-4 border-b border-white/30">
+            <div>
+                <a href="">
+                    <img width="200px" src="{{ Vite::asset('resources/images/logo.png') }}" alt="">
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
+
+        <main class="px-10 mt-10 max-w-[986px] mx-auto">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,16 @@
+@props(['tag', 'size' => 'base'])
+
+@php
+
+    $classes = 'bg-white/10 hover:bg-white/25 rounded-xl transition-colors duration-300';
+    if ($size === 'base') {
+        $classes .= ' px-5 py-1 text-sm';
+    }
+
+    if ($size === 'small') {
+        $classes .= ' px-3 py-1 text-xs';
+    }
+    
+@endphp
+
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
